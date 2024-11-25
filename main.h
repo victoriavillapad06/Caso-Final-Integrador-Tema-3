eee8f844c1d1fdb0105a6053f019cc3079526a2e
victoriavillapad06 <vvillpad@myuax.com>
2024-11-25 22:31:59 +0100

    ,

---
+++ main.h
@@ -0,0 +1,74 @@
+//
+// Created by victo on 25/11/2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+struct ColorConsole
+{
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto bg_white = "\033[47m";
+};
+
+struct ConsoleBox
+{
+    void new_text() {/*...*/}
+    void set_text(const string &text) { cout << text << endl; }
+};
+
+ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+
+void load_script(const char* filename, bool show_script = false)
+{
+    string script;
+    FILE* f = nullptr;
+    try
+    {
+        f = fopen(filename, "rb");
+        if (!f)
+        {
+            cerr << "error de apertura de " << filename << endl;
+            return;
+        }
+
+        int c;
+        char buf[4001];
+        while ((c = fread(buf, 1, 4000, f)) > 0)
+        {
+            buf[c] = 0;
+            script.append(buf);
+        }
+        fclose(f);
+        f = nullptr;
+
+        if (show_script)
+        {
+            cout << ColorConsole::fg_blue << ColorConsole::bg_white;
+            cout << script << endl;
+        }
+        consoleBox->new_text();
+        consoleBox->set_text(script);
+    }
+    catch (...)
+    {
+        cerr << "error durante la lectura del archivo" << endl;
+        if(f)
+            fclose(f);
+    }
+}
+
+void load_script()
+{
+    char filename[500];
+    printf("Archivo: ");
+    scanf("%499s", filename);
+    load_script(filename, true);
+}
